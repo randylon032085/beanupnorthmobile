93bf2e7416a731ae74ec4b8a5047f59bf356987e
randylon032085 <randylon032085@gmail.com>
2025-02-09 23:40:14 -0700

    update gradle

--- app/src/main/java/com/example/beanupnorth/Login.java
+++ app/src/main/java/com/example/beanupnorth/Login.java
@@ -1,6 +1,10 @@
 package com.example.beanupnorth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+public class Login extends AppCompatActivity {
+    private GoogleSignInClient googleSignInClient;
+    Button googleLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        googleLoginButton = findViewById(R.id.button4);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Google Login
+        googleLoginButton.setOnClickListener(view -> signInWithGoogle());
+
+        // Google Sign-In Config
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
     }
+
+
+
+//    public void signInGoogle (View v){
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken(getString(R.string.default_web_client_id))
+//                .requestEmail()
+//                .build();
+//        googleSignInClient = GoogleSignIn.getClient(this, gso);
+//        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+//    }
+private void signInWithGoogle() {
+    Intent signInIntent = googleSignInClient.getSignInIntent();
+    startActivityForResult(signInIntent, 100);
+}
+
 }
